b33bfe803e8caf2bace456dd50d1d597a833a3df
anagpure28 <aniketnagpureofficial@gmail.com>
2023-07-29 01:44:12 +0530

    Interview screen updated

--- front-end/src/Pages/SideBar2.jsx
+++ front-end/src/Pages/SideBar2.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import "../Styles/ScreenPage.css";
 
 const SideBar2 = () => {
   return (
-    <div className='sidebar2-main-cont'>
+    <div className="sidebar2-main-cont">
+      <div>
         <div>
+          <button>Check your answer</button>
+          <button>Start the Interview</button>
+          <button>End the Interview</button>
         </div>
-        <div>
-            <div></div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar2
+export default SideBar2;
